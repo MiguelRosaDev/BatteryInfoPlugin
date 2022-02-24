6024a78019776a6f8c2648d7df05146170e1ebfd
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2022-02-24 09:04:29 +0000

    Update and rename MyStuff.js to DeviceInfoPlugin.js

---
+++ www/DeviceInfoPlugin.js
@@ -0,0 +1,11 @@
+var exec = require("cordova/exec");
+
+var PLUGIN_NAME = "Idfa";
+
+exports.getInfo = function (resolve, reject) {
+  exec(resolve, reject, PLUGIN_NAME, "getInfo ", []);
+};
+
+exports.requestPermission = function (resolve, reject) {
+  exec(resolve, reject, PLUGIN_NAME, "requestPermission", []);
+};
