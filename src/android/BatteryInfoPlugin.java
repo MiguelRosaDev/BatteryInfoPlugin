b7b311b8e3cdb28370446f4d602138bc24e6ae36
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2022-03-02 12:55:56 +0000

    Update BatteryInfoPlugin.java

--- src/android/BatteryInfoPlugin.java
+++ src/android/BatteryInfoPlugin.java
@@ -1,6 +1,8 @@
 package com.cordova.plugin.BatteryInfoPlugin;
 
 import android.os.BatteryManager;
+import android.content.IntentFilter;
+import android.content.Intent;
 import android.content.Context;
 
 import org.apache.cordova.CordovaPlugin;
@@ -39,8 +41,13 @@ public class BatteryInfoPlugin extends CordovaPlugin {
           try {	
           Log.d(TAG, "getCapacity");
 		  
+	  IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+	  Intent batteryStatus = context.registerReceiver(null, ifilter);
+	  int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+	  int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+	  float batteryPct = level / (float) scale;
 		  
-	  callbackContext.success("");
+	  callbackContext.success(Math.round(batteryPct * 100));
 		  
           } catch (Exception e) {
             callbackContext.error(e.getMessage());
