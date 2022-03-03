408f3ce43c160bc74d6da59e52ca05d24c8115fb
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2022-03-03 08:30:12 +0000

    Update BatteryInfoPlugin.js

--- www/BatteryInfoPlugin.js
+++ www/BatteryInfoPlugin.js
@@ -3,7 +3,7 @@ var exec = require("cordova/exec");
 var PLUGIN_NAME = "BatteryInfoPlugin";
 
 exports.getPercentage = function (resolve, reject) {
-  exec(resolve, reject, PLUGIN_NAME, "getPercentage ", []);
+  exec(resolve, reject, PLUGIN_NAME, "getPercentage", []);
 };
 
 exports.getStatus = function (resolve, reject) {
